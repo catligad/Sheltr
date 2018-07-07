64cddece9cdc2342a724fef350bd7a0e5e0a037b
Catherine Ligad <kateligad@gmail.com>
2018-07-07 11:45:25 -0700

    started basic server

---
+++ server/apiKey.js
@@ -0,0 +1,1 @@
+module.exports = '8c6f8f8c7ad7e2d0ac89fa3c6ca04f6c';
---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const axios = require('axios');
+const parser = require('body-parser');
+const apiKey = require('./apiKey')
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.listen(PORT);
+app.use(morgan('tiny'));
+app.use(parser.json());
+
+app.get('/api/dogs', (req, res) => {
+  axios({
+    method: 'GET',
+    url: `http://api.petfinder.com/pet.find`,
+    params: {
+      key: apiKey,
+      format: 'json',
+      location: 94158,
+    }})
+  .then((response) => {
+    res.send({
+      data: response.data.petfinder.pets,
+    });
+  })
+  .catch( err => console.log(err));
+});
